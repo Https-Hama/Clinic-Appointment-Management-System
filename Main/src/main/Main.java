bb22a2731357f47615145a75bd76923d84c7a0fe
Hamma <zttiwolf255@gmail.com>
2026-06-19 20:53:27 +0300

    finishing patient class

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -6,11 +6,14 @@ public class Main {
         Doctor d1 = new GeneralPractitioner(101, "Ali");
         Doctor d2 = new Specialist(102, "Sara");
         Doctor d3 = new Consultant(103, "Ahmed");
-        
-        
-        d1.displayInfo(); 
-        d2.displayInfo(); 
-        d3.displayInfo(); 
+
+        d1.displayInfo();
+        d2.displayInfo();
+        d3.displayInfo();
+
+        Patient p1 = new Patient(1, "Ahmed", 22, "07701234567");
+
+        p1.displayInfo();
     }
 
 }
--- Main/src/main/Patient.java
+++ Main/src/main/Patient.java
@@ -1,5 +1,58 @@
 package main;
 
 public class Patient {
+    private int patientID;
+    private String name;
+    private int age;
+    private String phone;
+
+    public Patient(int patientID, String name, int age, String phone) {
+        this.patientID = patientID;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    
+    
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+     public void displayInfo()
+    {
+        System.out.println("Patient ID : " + patientID);
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Phone      : " + phone);
+    }
     
 }
